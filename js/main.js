cab96aeb4a5d826f404dfe3eb5f3717c834df3a1
HMejia01 <34866955+HMejia01@users.noreply.github.com>
2021-12-19 16:14:15 -0600

    Initial push

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const fetchUsers = async () =>
+  await (await fetch('/.netlify/functions/getusers')).json()
+
+fetchUsers()
+  .then((data) => {
+    userList = document.querySelector('#users')
+
+    console.log(data)
+    document.data = data
+
+    data.forEach(user => {
+        const li = document.createElement('li')
+        li.className = 'list-group-item'
+        const link = document.createElement('a')
+        link.appendChild(document.createTextNode(user.login))
+        link.href = user.html_url
+        link.target = '_blank'
+        li.appendChild(link)
+        userList.appendChild(li)
+    })
+  })
+  .catch((err) => {
+    
+    const li = document.createElement('li')
+    li.className = 'list-group-item'
+    // const link = document.createElement('a')
+    // link.appendChild(document.createTextNode(user.login))
+    // link.href = user.html_url
+    // link.target = '_blank'
+    // li.appendChild(link)
+    li.innerText = err.message
+    userList.appendChild(li)    
+
+  })
